693cd7113da093d4d95c7e22f4929f9f3bce948b
osho81 <ossi.hour@gmail.com>
2022-10-28 13:42:03 +0200

    Clean comments, unused code etc.

--- src/main/java/com/osho/twserviceregistry/TwServiceRegistryApplication.java
+++ src/main/java/com/osho/twserviceregistry/TwServiceRegistryApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/*Eureka server setup project
+ * Connected Eureka clients/microservices:
+ 		* cloud api gateway
+ 		* car rental project
+ 		* exchange service
+*/
 @EnableEurekaServer
 @SpringBootApplication
 public class TwServiceRegistryApplication {
